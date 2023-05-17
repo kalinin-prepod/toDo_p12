ba83d478d4db2d29a52610a9642096698ea553d4
Teacher <Teacher@R7T1>
2023-05-17 15:34:30 +0300

    Добавьте файлы проекта.

---
+++ toDo_v2/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace toDo_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelTasks = new System.Windows.Forms.Panel();
+            this.panelLeft.SuspendLayout();
+            this.panelTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panelLeft.Controls.Add(this.button3);
+            this.panelLeft.Controls.Add(this.button2);
+            this.panelLeft.Controls.Add(this.button1);
+            this.panelLeft.Controls.Add(this.button4);
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 100);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(185, 350);
+            this.panelLeft.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Image = global::toDo_v2.Properties.Resources.img_unchecked;
+            this.button3.Location = new System.Drawing.Point(0, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 48);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Show Completed";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Image = global::toDo_v2.Properties.Resources.img_unchecked;
+            this.button2.Location = new System.Drawing.Point(0, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Show Completed";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Image = global::toDo_v2.Properties.Resources.img_unchecked;
+            this.button1.Location = new System.Drawing.Point(0, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Show Completed";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Image = global::toDo_v2.Properties.Resources.img_unchecked;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 48);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Show Completed";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackgroundImage = global::toDo_v2.Properties.Resources.bg11;
+            this.panelTop.Controls.Add(this.label1);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(800, 100);
+            this.panelTop.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Tasks";
+            // 
+            // panelTasks
+            // 
+            this.panelTasks.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panelTasks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTasks.Location = new System.Drawing.Point(185, 100);
+            this.panelTasks.Name = "panelTasks";
+            this.panelTasks.Size = new System.Drawing.Size(615, 350);
+            this.panelTasks.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelTasks);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.panelTop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelLeft.ResumeLayout(false);
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelTasks;
+    }
+}
+
---
+++ toDo_v2/taskTile.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace toDo_v2
+{
+    public class taskTile
+    {
+        private Panel panel;
+        private Button buttonDone;
+        private Button buttonDelete;
+        private Label labelTitle;
+        private Label labelText;
+
+        private Panel parentPanel;
+        private string title;
+        private string text;
+
+        public taskTile(Panel parentPanel, string title, string text)
+        {
+            this.panel = new Panel();
+            this.labelText = new Label();
+            this.labelTitle = new Label();
+            this.buttonDelete = new Button();
+            this.buttonDone = new Button();
+
+            parentPanel.Controls.Add(panel);
+            this.panel.Controls.Add(labelText);
+            this.panel.Controls.Add(labelTitle);
+            this.panel.Controls.Add(buttonDone);
+            this.panel.Controls.Add(buttonDelete);
+
+            //панель текущего таска
+            this.panel.BackColor = Color.Transparent;
+            this.panel.Dock = DockStyle.Top;
+            this.panel.Size = new Size(0, 70);
+
+
+            //labelText
+            this.labelText.BackColor = Color.Transparent;
+            this.labelText.Dock = DockStyle.Top;
+            this.labelText.Font = new Font("Bahnschrift SemiLight SemiConde", 11F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(204)));
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new Size(47, 20);
+            this.labelText.Text = text;
+
+            //labelTitile
+            this.labelTitle.BackColor = Color.Transparent;
+            this.labelTitle.Dock = DockStyle.Top;
+            this.labelTitle.Font = new Font("Bahnschrift SemiLight SemiConde", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(204)));
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new Size(47, 20);
+            this.labelTitle.Text = title;
+
+
+
+
+            // buttonDone
+            // 
+            this.buttonDone.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonDone.FlatAppearance.BorderSize = 0;
+            this.buttonDone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDone.Image = global::toDo_v2.Properties.Resources.done;
+            this.buttonDone.Name = "buttonDone";
+            this.buttonDone.Size = new System.Drawing.Size(81, 350);
+            this.buttonDone.UseVisualStyleBackColor = true;
+
+
+            // buttonDelete
+            // 
+            this.buttonDelete.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonDelete.FlatAppearance.BorderSize = 0;
+            this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDelete.Image = global::toDo_v2.Properties.Resources.icons8_x_48;
+            this.buttonDelete.Name = "buttonDone";
+            this.buttonDelete.Size = new System.Drawing.Size(81, 350);
+            this.buttonDelete.UseVisualStyleBackColor = true;
+
+        }
+
+    }
+}
